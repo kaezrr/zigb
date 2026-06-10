15752b4e86e71297a94ec6ccf7da78020d4ce3a8
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-06-10 22:10:17 +0530

    add mocked memory for tests

--- src/Memory.zig
+++ src/Memory.zig
@@ -18,16 +18,3 @@ pub fn read(self: *Memory, at: u16) u8 {
 pub fn write(self: *Memory, at: u16, byte: u8) void {
     self.bytes[at] = byte;
 }
-
-// /// Mocked 64KB memory bus for testing purposes...
-// pub const FlatMemory = struct {
-//     bytes: [0x10000]u8 = undefined,
-//
-//     pub fn read(self: *Memory, at: u16) u8 {
-//         return self.bytes[at];
-//     }
-//
-//     pub fn write(self: *Memory, at: u16, byte: u8) void {
-//         self.bytes[at] = byte;
-//     }
-// };
